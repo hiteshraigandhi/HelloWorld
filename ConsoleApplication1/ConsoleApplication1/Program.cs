a6ee1924b90439846eb849fb32f41684524cd3c9
HiteshRaigandhi <hiteshr@microsoft.com>
2016-02-29 14:54:54 -0800

    update from laptop

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World Update");
+            Console.WriteLine("Hello World Update2 from different machine");
         }
     }
 }
